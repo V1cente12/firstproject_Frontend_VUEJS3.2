7574cb591bacd26c3a58fd8e12d5cdd141453311
V1cente12 <jvicentecv@gmail.com>
2024-10-29 21:19:43 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyCustomComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import MyCustomComponent from './components/MyCustomComponent.vue'; // Importa el nuevo nombre
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyCustomComponent // Usa el nuevo nombre aquí
   }
 }
 </script>
---
+++ src/components/MyCustomComponent.vue
@@ -0,0 +1,24 @@
+<!-- src/components/MyCustomComponent.vue -->
+<template>
+  <div>
+    <h1>Hola desde MiComponente</h1>
+    <p>{{ mensaje }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCustomComponent', // Nombre cambiado a multi-palabra
+  data() {
+    return {
+      mensaje: 'Este es un componente personalizado en Vue'
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
